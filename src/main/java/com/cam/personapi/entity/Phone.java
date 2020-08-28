40ea0b40e4feee6691e0b5c82e04ab3783093aae
Dario Campagna <dario@localhost.localdomain>
2020-08-28 13:18:20 -0300

    Mapeando todas classes.

--- src/main/java/com/cam/personapi/entity/Phone.java
+++ src/main/java/com/cam/personapi/entity/Phone.java
@@ -1,5 +1,13 @@
 package com.cam.personapi.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.cam.personapi.enums.PhoneType;
 
 import lombok.AllArgsConstructor;
@@ -7,15 +15,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private PhoneType type;
 	
+	@Column(nullable = false)
 	private String number;
 }
