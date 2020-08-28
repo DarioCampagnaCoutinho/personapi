c2d967b9f8b0963c0752b7dae04c48beaa80c233
Dario Campagna <dario@localhost.localdomain>
2020-08-28 12:58:04 -0300

    Criando classes e enum.

---
+++ src/main/java/com/cam/personapi/entity/Phone.java
@@ -0,0 +1,21 @@
+package com.cam.personapi.entity;
+
+import com.cam.personapi.enums.PhoneType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+	
+	private Long id;
+	
+	private PhoneType type;
+	
+	private String number;
+}
