17b1f58c91432304c320148211ed072437bcea2a
Dario Campagna <dario@localhost.localdomain>
2020-08-29 12:36:29 -0300

    Criando classes DTO, importando MapStruct e inserindo uma pessoa. Ajuste das classes PersonService e PersonController.

--- src/main/java/com/cam/personapi/controller/PersonController.java
+++ src/main/java/com/cam/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.cam.personapi.controller;
 
+import com.cam.personapi.dto.request.PersonDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cam.personapi.dto.response.MessageResponseDto;
 import com.cam.personapi.entity.Person;
 import com.cam.personapi.service.PersonService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -25,8 +28,8 @@ public class PersonController {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public MessageResponseDto personCreate(@RequestBody Person person) {
-		return personService.createPerson(person);
+	public MessageResponseDto personCreate(@RequestBody @Valid PersonDTO personDTO) {
+		return personService.createPerson(personDTO);
 	}
 	
 }
--- src/main/java/com/cam/personapi/service/PersonService.java
+++ src/main/java/com/cam/personapi/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.cam.personapi.service;
 
+import com.cam.personapi.dto.request.PersonDTO;
+import com.cam.personapi.mapper.PersonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,16 @@ public class PersonService {
 	
 	private PersonRepository personRepository;
 
+	private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
 	@Autowired
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
 	}
 	
-	public MessageResponseDto createPerson(Person person) {
-		Person savedPerson = personRepository.save(person);
+	public MessageResponseDto createPerson(PersonDTO personDTO) {
+		Person personToSaved = personMapper.toModel(personDTO);
+		Person savedPerson = personRepository.save(personToSaved);
 		return MessageResponseDto.builder()
 				.message("Created person with ID : " + savedPerson.getId())
 				.build();
